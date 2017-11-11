76eb4eb84497b2d4a1cf1328c9196451b6bc8cac
Lili <liriot.production@gmail.com>
2017-11-11 13:23:23 +0100

    Added support for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912221350) do
+ActiveRecord::Schema.define(version: 20171110170202) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
